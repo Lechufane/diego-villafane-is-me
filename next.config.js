40ce0c5fd2173af60179f5610f975e5e81d95a5e
Lechufane <diego.villafane94@gmail.com>
2023-04-18 15:03:39 -0300

    commit

--- next.config.js
+++ next.config.js
@@ -5,12 +5,7 @@ const nextConfig = {
   },
   compiler: {
     styledComponents: true,
-  },
-  env: {
-    YOUR_SERVICE_ID:'service_2gzteos',
-    YOUR_TEMPLATE_ID:'template_gqxf38p',
-    YOUR_PUBLIC_KEY:'-gbYQsi7djBUFOZ2_'
-  },
+  }
 }
 
 module.exports = nextConfig
