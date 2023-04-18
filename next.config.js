27ce1e81c39d7140c9787e0ad4c96cd5726f5d83
Lechufane <diego.villafane94@gmail.com>
2023-04-18 15:27:39 -0300

    commit

--- next.config.js
+++ next.config.js
@@ -1,15 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
+  experimental: {},
   compiler: {
     styledComponents: true,
-  },
-  images: {
-    // here we can add the domains we want to use
-    domains: ['diegovillafane.com'],
-  },
+  }
 }
 
 module.exports = nextConfig
